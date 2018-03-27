fffef2cb1e3cec9bc4d9ea711281b7d9f09fea7a
Aaron Tesch <aaron.tesch@target.com>
2018-03-27 10:51:10 -0500

    replaced gradlew with gradle

--- start_app.sh
+++ start_app.sh
@@ -4,6 +4,6 @@ docker run -itd --rm --name mongo_myretail -p 27017:27017 mongo
 
 mongoimport --db Products --collection product --drop --jsonArray --file ./import.json
 
-./gradlew bootrun
+gradle bootrun
 
 docker stop mongo_myretail
